9f1af17ed6ca1cea98ff8c656754a16d52942655
SandObserver <armin.pblc@gmail.com>
2026-07-01 09:03:53 -0400

    startup banner

--- api/src/server.js
+++ api/src/server.js
@@ -14,5 +14,26 @@ const { dispatch } = require('./router');
 
 http.createServer(dispatch).listen(PORT, () => {
   const { CONFIG_PATH, ICONS_PATH } = require('./config');
-  log.info('started', { port:PORT, config:CONFIG_PATH, icons:ICONS_PATH, node:process.version });
+  const { getRegistry } = require('./widgets');
+  const pkg = require('../package.json');
+  const widgets = Object.keys(getRegistry());
+  const W = 47;
+  const bar = c => c + '─'.repeat(W) + (c === '┌' ? '┐' : '┘');
+  const mid = t => '│ ' + t.padEnd(W - 2) + ' │';
+  const wlist = widgets.slice(0, 4).join(', ') + (widgets.length > 4 ? ', ...' : '');
+  const lines = [
+    '',
+    '  ' + bar('┌'),
+    '  ' + mid('STACKYARD'),
+    '  ' + mid('self-hosted dashboard · v' + pkg.version),
+    '  ' + bar('└'),
+    `  ➜  Web UI:    http://localhost:${PORT}`,
+    `  ➜  Config:    ${CONFIG_PATH}`,
+    `  ➜  Icons:     ${ICONS_PATH}`,
+    `  ➜  Widgets:   ${widgets.length} loaded (${wlist})`,
+    `  ➜  Node:      ${process.version}`,
+    '',
+  ];
+  console.log(lines.join('\n'));
+  log.info('started', { port:PORT, version:pkg.version, config:CONFIG_PATH, icons:ICONS_PATH, node:process.version });
 });
